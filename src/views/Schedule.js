240606628576cdcc720c886a5cdca068e5e200fd
Jam Silvestre <pjdeesilvestre@gmail.com>
2019-02-24 09:45:15 +0800

    added calendar

--- src/views/Schedule.js
+++ src/views/Schedule.js
@@ -1,12 +1,105 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import BigCalendar from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = BigCalendar.momentLocalizer(moment);
 
 class Schedule extends Component {
-  state = {};
+  state = {
+    userType: "teacher",
+    events: [
+      {
+        id: 8,
+        title: "Meeting",
+        start: new Date(2019, 1, 17, 14, 0, 0, 0),
+        end: new Date(2019, 1, 17, 15, 0, 0, 0),
+        status: "closed"
+      },
+      {
+        id: 9,
+        title: "Class",
+        start: new Date(2019, 1, 18, 17, 0, 0, 0),
+        end: new Date(2019, 1, 18, 17, 30, 0, 0),
+        desc: "Most important meal of the day",
+        status: "closed"
+      },
+      {
+        id: 10,
+        title: "Class",
+        start: new Date(2019, 1, 19, 17, 0, 0, 0),
+        end: new Date(2019, 1, 19, 17, 30, 0, 0),
+        desc: "Most important meal of the day",
+        status: "available"
+      }
+    ]
+  };
+
+  handleUserTypeChange = e => {
+    console.log(e.target.value);
+    this.setState({ userType: e.target.value });
+  };
+
+  handleEventSelectEvent = event => {
+    console.log(event, "event");
+    const { userType } = this.state;
+    console.log(userType, "event");
+
+    if (userType === "student") {
+      alert("class booked!");
+    }
+  };
+
+  handleEventSelectSlot = ({ start, end }) => {
+    console.log(start, end, "PARAMS");
+    const { userType, events } = this.state;
+    console.log(userType, "type");
+    if (userType === "teacher" || userType === "admin") {
+      const title = window.prompt("New Event name");
+      this.setState({
+        events: [
+          ...events,
+          {
+            start,
+            end,
+            title
+          }
+        ]
+      });
+      return;
+    }
+
+    alert(
+      "not a class for this sched yet. contact administrator for a clss request"
+    );
+  };
 
   render() {
     console.log(this.props);
-    return <div className="content">test</div>;
+    const { events, userType } = this.state;
+    return (
+      <div className="content">
+        <select value={userType} onChange={this.handleUserTypeChange}>
+          <option value="teacher">Teacher</option>
+          <option value="student">Student</option>
+        </select>
+        <br />
+        <br />
+        <BigCalendar
+          selectable
+          localizer={localizer}
+          events={events}
+          defaultView={BigCalendar.Views.WEEK}
+          defaultDate={new Date()}
+          onSelectSlot={this.handleEventSelectSlot}
+          onSelectEvent={this.handleEventSelectEvent}
+          eventPropGetter={event => ({
+            className: event.status
+          })}
+        />
+      </div>
+    );
   }
 }
 
